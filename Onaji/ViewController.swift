fd06f1ecb8eb1f836db91059228e39fb3635fdb0
alexluuuu <aeksanthr@gmail.com>
2015-07-23 19:48:40 -0700

    Test
    Test

---
+++ Onaji/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Onaji
+//
+//  Created by Matthew Li on 7/21/15.
+//  Copyright (c) 2015 Same. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func loginPage()
+    {
+// this is to test parallel changes on diff files
+        // more testing
+    }
+
+}
+
